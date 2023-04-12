e6a3b1a536a9d90e49e7b66eb352f6065a92de00
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-12 07:12:01 +0100

    display all task controller created

--- comtroller/taskController.js
+++ comtroller/taskController.js
@@ -1,6 +1,17 @@
 import Post from "../model/taskModel.js"
 
 //displayTask
+export const displayAllTask = async (req, res) => {
+    try {
+        const allTask = await Post.find({});
+        res.json({
+            status: "success",
+            data: allTask
+        })
+    } catch (error) {
+        res.json(error.message) 
+    }
+}
 
 //addTask
 export const addTaskController = async (req, res) => {
