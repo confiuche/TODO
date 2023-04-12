b15f6ddb07695535e44e3817b796a36f5cda14ad
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-04-12 07:12:51 +0100

    display all route created

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,11 @@
 import express from "express"
-import { addTaskController } from "../comtroller/taskController.js"
+import { addTaskController, displayAllTask } from "../comtroller/taskController.js"
 
 const taskRoutes = express.Router()
 
 
 //displayTask
-taskRoutes.get("")
+taskRoutes.get("",displayAllTask)
 //addTask
 taskRoutes.post("/addTask",addTaskController)
 //deleteTask
